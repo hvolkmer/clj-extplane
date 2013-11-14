d2a8743a604cb50a57fa1cefa32315c44fe23ef3
Hendrik Volkmer <hvolkmer@gmail.com>
2013-11-14 07:40:49 +0100

    Unified indentation

--- src/extplane/core.clj
+++ src/extplane/core.clj
@@ -10,13 +10,13 @@
 ;; remove this notice, or any other, from this software.
 
 (ns extplane.core
-    (:import (java.net Socket)
-             (java.io PrintWriter InputStreamReader BufferedReader))
-    (:require [instaparse.core :as insta]
-              [extplane.mappings :as mappings]
-              [clojure.data.codec.base64 :as b64]
-              [clojure.java.io :as io])
-    (:use  [clojure.core.match :only (match)]))
+  (:import (java.net Socket)
+           (java.io PrintWriter InputStreamReader BufferedReader))
+  (:require [instaparse.core :as insta]
+            [extplane.mappings :as mappings]
+            [clojure.data.codec.base64 :as b64]
+            [clojure.java.io :as io])
+  (:use  [clojure.core.match :only (match)]))
 
 (def xplane {:name "localhost" :port 51000})
 (def ^:dynamic *debug* false)
@@ -46,7 +46,7 @@ type_integer = 'ui'
 type_float = 'uf'
 type_double = 'ud'
 type_float_array = 'ufa'
-type_data = 'ub'  
+type_data = 'ub'
 "))
 
 (declare conn-handler)
@@ -66,15 +66,15 @@ type_data = 'ub'
     (.flush)))
 
 (defn conn-handler [conn]
-   (while (nil? (:exit @conn))
+  (while (nil? (:exit @conn))
     (let [msg (.readLine (:in @conn))]
       (if *debug*
         (println (str "DEBUG: >" msg "<")))
-       (if (nil? msg)
-           (dosync (alter conn merge {:exit true}))
-           (parse-input msg)))))
+      (if (nil? msg)
+        (dosync (alter conn merge {:exit true}))
+        (parse-input msg)))))
 
-;;; This is the actual external API 
+;;; This is the actual external API
 (defn subscribe-dataref [conn refname]
   (write conn (str "sub " refname)))
 
@@ -83,7 +83,7 @@ type_data = 'ub'
 
 (defn set-update-interval [conn hz]
   (write conn (str "extplane-set update_interval " (float(/ 1 hz)))))
-  
+
 (defn press-down [conn button]
   (write conn (str "but " (mappings/get-button-mapping button))))
 
@@ -92,7 +92,7 @@ type_data = 'ub'
 
 (defn toggle [conn key]
   (write conn (str "key " (mappings/get-key-mapping key))))
-    
+
 (defn set-dataref [conn refname value]
   ;; It is only possible to write to a data-ref if it is subscribed. We don't keep track
   ;; of subscriptions so we just issue a subscription before writing
@@ -104,17 +104,17 @@ type_data = 'ub'
 
 (defn add-dataref-handler!
   "Adds a handler function for a data ref. The handler function will get called if
-   the value inside X-Plane is changed and the the client has subscribed to 
+   the value inside X-Plane is changed and the the client has subscribed to
    this data-ref. This function does NOT subscribe to the dataref."
   [dataref handler-fn]
   (dosync (alter dataref-handlers assoc dataref handler-fn)))
 
-(defn remove-dataref-handler! 
+(defn remove-dataref-handler!
   "Removes the handler function. This does not unsubscribe the data-ref"
   [dataref]
   (dosync (alter dataref-handlers dissoc dataref)))
 
-(defn- handle-dataref 
+(defn- handle-dataref
   "Handles the callback by calling the registered function with the changed value.
    If a data ref is subscribed and no handler is defined, a noop function will be called."
   [ref-name value]
@@ -127,7 +127,7 @@ type_data = 'ub'
   (vec (map parse-number (rest array))))
 
 (defn- decode64 [encoded]
-    (String. (b64/decode (.getBytes encoded)) "UTF-8"))
+  (String. (b64/decode (.getBytes encoded)) "UTF-8"))
 
 (defn- parse-input
   "uses the extplane parser to parse the message and, if the message is data ref, calls the handler fn.
@@ -135,11 +135,11 @@ type_data = 'ub'
    is base64 encoded to binary."
   [msg]
   (let [parsed-message (match (extplane-parser msg)
-                             [:extplane_protocol [:data_ref [:data_dataref [:type_data "ub"] [:dataref_name data_ref] [:base64data content]]]] {:ref data_ref :content (decode64 content)}
-                             [:extplane_protocol [:data_ref [:float_array_dataref [:type_float_array "ufa"] [:dataref_name data_ref] array]]] {:ref data_ref :content (value-array array)}
-                             [:extplane_protocol [:data_ref [:integer_array_dataref [:type_integer_array "uia"] [:dataref_name data_ref] array]]] {:ref data_ref :content (value-array array)} 
-                             [:extplane_protocol [:data_ref [_ _ [:dataref_name data_ref] value]]] {:ref data_ref :content (parse-number value)}
-                             [:extplane_protocol [:version [:integer version]]] {:version version}
-                             )]
-       (if (parsed-message :ref)
-         (handle-dataref (parsed-message :ref) (parsed-message :content)))))
+                              [:extplane_protocol [:data_ref [:data_dataref [:type_data "ub"] [:dataref_name data_ref] [:base64data content]]]] {:ref data_ref :content (decode64 content)}
+                              [:extplane_protocol [:data_ref [:float_array_dataref [:type_float_array "ufa"] [:dataref_name data_ref] array]]] {:ref data_ref :content (value-array array)}
+                              [:extplane_protocol [:data_ref [:integer_array_dataref [:type_integer_array "uia"] [:dataref_name data_ref] array]]] {:ref data_ref :content (value-array array)}
+                              [:extplane_protocol [:data_ref [_ _ [:dataref_name data_ref] value]]] {:ref data_ref :content (parse-number value)}
+                              [:extplane_protocol [:version [:integer version]]] {:version version}
+                              )]
+    (if (parsed-message :ref)
+      (handle-dataref (parsed-message :ref) (parsed-message :content)))))
--- test/extplane/core_test.clj
+++ test/extplane/core_test.clj
@@ -4,100 +4,99 @@
         extplane.core))
 
 (defn read-fake-conn [conn]
-   (.toString (conn :outstream)))
+  (.toString (conn :outstream)))
 
 (defn fake-in-conn [input]
-    (let [instream (ByteArrayInputStream. (.getBytes input))
-         in (BufferedReader. (InputStreamReader. instream))] 
-             (ref {:in in})))
+  (let [instream (ByteArrayInputStream. (.getBytes input))
+        in (BufferedReader. (InputStreamReader. instream))]
+    (ref {:in in})))
 
 (defn fake-conn []
   (let [outstream (ByteArrayOutputStream.)
-        out (PrintWriter. outstream)] 
-       (ref {:out out :outstream outstream})))
+        out (PrintWriter. outstream)]
+    (ref {:out out :outstream outstream})))
 
 (defn conn-test-wrapper [fn-to-execute & args]
-    (let [conn (fake-conn)]
-         (apply fn-to-execute conn args)
-         (read-fake-conn conn)))
+  (let [conn (fake-conn)]
+    (apply fn-to-execute conn args)
+    (read-fake-conn conn)))
 
 (def test-ref (ref {}))
 
 (defn ref-handling-wrapper [ref-name in-string]
-    (dosync (ref-set test-ref {}))
-    (add-dataref-handler! ref-name (fn [x] (dosync (alter test-ref assoc :test x)) ) )
-    (conn-handler (fake-in-conn in-string)))
+  (dosync (ref-set test-ref {}))
+  (add-dataref-handler! ref-name (fn [x] (dosync (alter test-ref assoc :test x)) ) )
+  (conn-handler (fake-in-conn in-string)))
 
 ;; TODO: What to do when no ref supplied
 (deftest test-subscribe-dataref
   (testing "subsciption"
-  (is (= "sub some/dataref\r\n" (conn-test-wrapper subscribe-dataref "some/dataref")))))
+    (is (= "sub some/dataref\r\n" (conn-test-wrapper subscribe-dataref "some/dataref")))))
 
 (deftest test-unsubscribe-dataref
   (testing "remove subsciption"
-  (is (= "unsub some/dataref\r\n" (conn-test-wrapper unsubscribe-dataref "some/dataref")))))
+    (is (= "unsub some/dataref\r\n" (conn-test-wrapper unsubscribe-dataref "some/dataref")))))
 
 ;; TODO: What to do on no interval
 (deftest test-set-update-interval
-    (testing "update interval"
+  (testing "update interval"
     (is (= "extplane-set update_interval 0.02\r\n" (conn-test-wrapper set-update-interval 50)))))
 
 (deftest test-press-down
   (testing "press down"
-  (is (= "but 81\r\n" (conn-test-wrapper press-down :joy_TOGA)))))
+    (is (= "but 81\r\n" (conn-test-wrapper press-down :joy_TOGA)))))
 
 ;; TODO: What to do on unkown button
 (deftest test-press-down-unkown
-   (testing "unkown button"
-   (is (= "but \r\n" (conn-test-wrapper press-down :unknown)))))
+  (testing "unkown button"
+    (is (= "but \r\n" (conn-test-wrapper press-down :unknown)))))
 
 (deftest test-release
   (testing "release"
-  (is (= "rel 81\r\n" (conn-test-wrapper release :joy_TOGA)))))
+    (is (= "rel 81\r\n" (conn-test-wrapper release :joy_TOGA)))))
 
 (deftest test-toggle
   (testing "toggle"
-  (is (= "key 0\r\n" (conn-test-wrapper toggle :key_pause)))))
+    (is (= "key 0\r\n" (conn-test-wrapper toggle :key_pause)))))
 
 (deftest test-disconnect
   (testing "disconnect"
-  (is (= "disconnect\r\n" (conn-test-wrapper disconnect)))))
+    (is (= "disconnect\r\n" (conn-test-wrapper disconnect)))))
 
 (deftest test-set-dataref
   (testing "set-dataref"
-  (is (= "sub some/dataref\r\nset some/dataref 100\r\n" (conn-test-wrapper set-dataref "some/dataref" 100)))))
+    (is (= "sub some/dataref\r\nset some/dataref 100\r\n" (conn-test-wrapper set-dataref "some/dataref" 100)))))
 
 (deftest test-connect-disconnect
-    (testing "disconnect"
+  (testing "disconnect"
     (is (= nil (conn-handler (fake-in-conn "EXTPLANE 1"))))))
 
 (deftest test-integer-ref
-    (testing "integer-ref"
+  (testing "integer-ref"
     (ref-handling-wrapper "acf_num_engines" "ui acf_num_engines 2")
     (is (= 2 (@test-ref :test)))))
 
 (deftest test-float-ref
-   (testing "float ref"
-   (ref-handling-wrapper "heading" "uf heading 2.23")
-   (is (= 2.23 (@test-ref :test)))))
+  (testing "float ref"
+    (ref-handling-wrapper "heading" "uf heading 2.23")
+    (is (= 2.23 (@test-ref :test)))))
 
 (deftest test-integer-array-ref
-   (testing "integer array ref"
-   (ref-handling-wrapper "N1_percent" "uia N1_percent [99,97]")
-   (is (= [99 97] (@test-ref :test)))))
+  (testing "integer array ref"
+    (ref-handling-wrapper "N1_percent" "uia N1_percent [99,97]")
+    (is (= [99 97] (@test-ref :test)))))
 
 (deftest test-float-array-ref
-   (testing "float array ref"
-   (ref-handling-wrapper "percent" "ufa percent [22.22,922.22]")
-   (is (= [22.22,922.22] (@test-ref :test)))))
+  (testing "float array ref"
+    (ref-handling-wrapper "percent" "ufa percent [22.22,922.22]")
+    (is (= [22.22,922.22] (@test-ref :test)))))
 
 (deftest test-double-ref
   (testing "double ref"
-   (ref-handling-wrapper "heading" "ud heading 223.23")
-   (is (= 223.23 (@test-ref :test)))))
+    (ref-handling-wrapper "heading" "ud heading 223.23")
+    (is (= 223.23 (@test-ref :test)))))
 
 (deftest test-binary-ref
   (testing "binary ref"
-  (ref-handling-wrapper "acf_descrip" "ub acf_descrip RXh0UGxhbmUgU2ltdWxhdGVkIENvbm5lY3Rpb24=")
-  (is (= "ExtPlane Simulated Connection" (@test-ref :test)))))
-
+    (ref-handling-wrapper "acf_descrip" "ub acf_descrip RXh0UGxhbmUgU2ltdWxhdGVkIENvbm5lY3Rpb24=")
+    (is (= "ExtPlane Simulated Connection" (@test-ref :test)))))
